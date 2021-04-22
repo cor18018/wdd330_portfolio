04167b4c7e7245920cb3570a6af4bdaba943a5ce
cor18018 <cor18018@byui.edu>
2021-04-21 22:02:53 -0600

    Week 1 notes and exercise

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-const links = [
-    {
-      label: "Week 1 Notes",
-      url: "index.html"
-    }
-  ]
+window.addEventListener("load", () => {
+  const links = [
+      {
+        label: "Week 1 Notes",
+        url: "week1/reading_notes.html"
+      }
+    ]
 
-var list = document.getElementById("list");
+  var list = document.getElementById("list");
 
-for(var i = 0; i < links.length; i++) {
-    let link = document.createElement("li");
-    let ref = document.createElement("a");
-    ref.innerHTML = links[i].label;
-    ref.setAttribute('href', links[i].url);
-    link.appendChild(ref);
-    console.log(link);
-    list.appendChild(link);
-}
+  for(var i = 0; i < links.length; i++) {
+      let link = document.createElement("li");
+      let ref = document.createElement("a");
+      ref.innerHTML = links[i].label;
+      ref.setAttribute('href', links[i].url);
+      link.appendChild(ref);
+      console.log(link);
+      list.appendChild(link);
+  }
+});
