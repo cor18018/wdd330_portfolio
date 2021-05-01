a62dd406984e66517b7e99b10e7fe7de1617b297
cor18018 <cor18018@byui.edu>
2021-05-01 15:39:14 -0600

    Team Activity

--- week2/js/adding.js
+++ week2/js/adding.js
@@ -1,23 +1,95 @@
-// window.addEventListener("load", () => {
-    function add(){
-        let input = document.getElementById("input").value;
-        document.getElementById("output").innerHTML = input;
-    }
-//   });
-  function numArg() {
-    let input = document.getElementById("input2").value;
-      var total = 0;
-      for (var i = 0; i <= input; i++) {
-        total += i;
-      }
-      document.getElementById("output2").innerHTML = total;
+// // window.addEventListener("load", () => {
+//     function add(){
+//         let input = document.getElementById("input").value;
+//         document.getElementById("output").innerHTML = input;
+//     }
+// //   });
+//   function numArg() {
+//     let input = document.getElementById("input2").value;
+//       var total = 0;
+//       for (var i = 0; i <= input; i++) {
+//         total += i;
+//       }
+//       document.getElementById("output2").innerHTML = total;
+//   }
+
+//   function final() {
+//     let input1 = parseFloat(document.getElementById("input").value);
+//     let input2 = parseFloat(document.getElementById("input2").value);
+//     let input3 = parseFloat(document.getElementById("input3").value);
+//     let  sum = input1 + input2 + input3;
+//     document.getElementById("output3").innerHTML = sum;
+
+// }
+function reqOne() {
+  // we can use getElementById to grab the correct input...but we are interested in the value of the input...not the input itself...so we tell it that.
+  const input = document.getElementById('req1Input').value;
+  // Here we want the actual element so no .value...not to mention that only input elements HAVE a .value
+  const outputElement = document.getElementById('req1Output');
+  //  The content for most elements (other than inputs) lives in innerHTML
+  outputElement.innerHTML = 'You entered: ' + input;
+}
+// req 2
+function reqTwo() {
+  const input = document.getElementById('req2Input').value;
+  const outputElement = document.getElementById('req2Output');
+  // make sure input is a number...we can convert it from the string we get from the input value to a number with parseInt.  If this fails it's not a number
+  const inputInt = parseInt(input);
+  if (inputInt !== NaN) {
+    outputElement.innerHTML = 'total: ' + sum(inputInt);
   }
+}
+function sum(number) {
+  let total = 0;
+  for (let i = 1; i <= number; i++) {
+    total += i;
+  }
+  return total;
+}
 
-  function final() {
-    let input1 = parseFloat(document.getElementById("input").value);
-    let input2 = parseFloat(document.getElementById("input2").value);
-    let input3 = parseFloat(document.getElementById("input3").value);
-    let  sum = input1 + input2 + input3;
-    document.getElementById("output3").innerHTML = sum;
+// req 3
+function add() {
+  const number1 = document.getElementById('req3Number1').value;
+  const number2 = document.getElementById('req3Number2').value;
+  const outputElement = document.getElementById('req3Output');
+  //  we need to check for valid input again...this time decimals would be ok...so lets use parseFloat instead of parseInt
+  const num1Float = parseFloat(number1);
+  const num2Float = parseFloat(number2);
+  if ((num1Float !== NaN) & (num2Float !== NaN)) {
+    const total = num1Float + num2Float;
+    outputElement.innerHTML = 'Total: ' + total;
+  }
+}
 
+// stretch solution
+function getNum(numId) {
+  const number = document.getElementById(numId).value;
+  const numberFloat = parseFloat(number);
+  if (numberFloat !== NaN) {
+    return numberFloat;
+  } else return 0;
+}
+function updateTotal(value) {
+  const outputElement = document.getElementById('stretchOutput');
+  outputElement.innerHTML = 'Total: ' + value;
+}
+// function declaration
+function add2(num1, num2) {
+  return num1 + num2;
+}
+// function expression
+const sub2 = function(num1, num2) {
+  return num1 - num2;
+};
+// arrow function
+const mult2 = (num1, num2) => num1 * num2;
+
+// example of using a callback to pull it all together
+function performOperation(operation) {
+  // the argument 'operation' is a function...so when we are ready we can call that function in our code below
+  const total = operation(
+    getNum('stretchNumber1'),
+    getNum('stretchNumber2')
+  );
+  updateTotal(total);
 }
