8dc530d1d165e0935486409ed7d9a81ab393ae48
cor18018 <cor18018@byui.edu>
2021-05-27 20:40:14 -0600

    Changes

---
+++ week5/ls.js
@@ -0,0 +1,3 @@
+function writeToLS(key, data) {
+    
+}
---
+++ week5/main.js
@@ -0,0 +1,5 @@
+import * as ls from './ls.js';
+import * as ut from './utilities.js';
+
+const tasks = document.getElementById("tasks");
+const input = document.getElementById("todoTask");
